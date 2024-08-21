e661da2e4ca1ef4eabde0e87678228d3025707e2
Mauli Waghmore <mauliwaghmore9696@gmail.com>
2024-08-21 22:51:54 +0530

    Implemented follower service

--- src/main/kotlin/example/com/service/FollowService.kt
+++ src/main/kotlin/example/com/service/FollowService.kt
@@ -39,4 +39,20 @@ class FollowService(
             )
         }
     }
+
+    suspend fun getFollowedToUser(userId: String): List<UserResponseItem> {
+        val followedToUser = followRepository.getFollowedToUser(userId)
+        val followerIds = followedToUser.map { it.followingUserId }
+        val users = userRepository.getUsers(followerIds)
+        return users.map { user ->
+            val isFollowing = followedToUser.find { it.followingUserId == user.id } != null
+            UserResponseItem(
+                userId = user.id,
+                username = user.username,
+                profilePictureUrl = user.profileImageUrl,
+                bio = user.bio,
+                isFollowing = isFollowing
+            )
+        }
+    }
 }
